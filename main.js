6882e8b3a7a78e50f68214820973f8ed5ad120b1
studio-dhri <252181032+studio-dhri@users.noreply.github.com>
2026-05-03 01:00:42 +0200

    Button accepts input, pixel changes color on hover

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
+button.addEventListener("click", newSlate)
+
+function newSlate() {
+    let sideLength = 16;
+    do {
+        sideLength = parseInt(prompt("Enter number of squares per side for the new grid (max 100)", "16"));
+    } while (sideLength > 100)
+    drawGrid(sideLength);
+}
+
+
+
 const squareDiv = document.createElement("div");
 squareDiv.classList.add("pixel");
+squareDiv.style.width = "100px";
+squareDiv.style.height = "100px";
 container.appendChild(squareDiv);
+
+squareDiv.addEventListener("mouseenter", () => squareDiv.classList.add("hovered"));
